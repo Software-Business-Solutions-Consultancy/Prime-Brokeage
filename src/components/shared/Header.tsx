a3f7e9b19659be35b0b70010ea0e42665bd6a60a
Alaketu Olamilekan <alaketuolamilekan02@gmail.com>
2025-12-08 17:35:06 +0100

    update app

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,11 +1,12 @@
 
 const Header = () => {
+
   return (
    <div className='fixed bg-white top-0 w-full mb-10' >
     <div className="relative pb-1">
  
      <p className='text-black px-[71px] font-bold font-lato py-4 flex justify-end gap-4 '> CORONATION </p> 
-  <div className="absolute left-0 right-0 bottom-0 h-[2px] bg-gradient-to-r from-[#FF8200] to-[#FF002B] rounded-full" />
+  <div className={`absolute left-0 right-0 bottom-0 h-[2px] rounded-full ${window.location.pathname.includes('customer') ? 'bg-[#BA68C8]' : 'bg-gradient-to-r from-[#FF8200] to-[#FF002B]'} `} />
 
    </div>
 </div>
